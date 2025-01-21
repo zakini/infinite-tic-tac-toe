a4e27fe093dc142c231cd89ac92b6f1aedbbfec6
Josh Wells <josh@parall.ax>
2025-01-21 19:00:08 +0000

    Removed default cruft

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,6 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 } satisfies Config;
