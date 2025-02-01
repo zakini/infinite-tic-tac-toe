4980c670679956a8a3233da90688dcd1871eb8aa
Josh Wells <josh@parall.ax>
2025-02-01 21:30:24 +0000

    Added option to concede or go deeper on win
    Closes #2

--- app/components/tic-tac-toe-board/index.tsx
+++ app/components/tic-tac-toe-board/index.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { useState } from 'react'
 import { create } from 'zustand'
 import { combine } from 'zustand/middleware'
 import { useShallow } from 'zustand/shallow'
-import { BoardState, FilledCellState, isBoardState } from './types'
+import { BoardState, FilledCellState, isBoardState, Win } from './types'
 import { findWin, initialiseBoardState, pickNestedBoardState, setBoardStateWithPath } from './utils'
 
 const useGameStore = create(combine(
@@ -16,8 +17,55 @@ const useGameStore = create(combine(
       boardState: setBoardStateWithPath(boardState, path, turn),
       turn: turn === FilledCellState.X ? FilledCellState.O : FilledCellState.X,
     })),
+    goDeeper: () => set(({ boardState }) => {
+      const initialState = initialiseBoardState()
+      // Embed the current board state into the centre (index 4) of a new board
+      const newState = [
+        ...initialState.slice(0, 4),
+        boardState,
+        ...initialState.slice(4 + 1),
+      ]
+
+      if (!isBoardState(newState)) {
+        throw new Error(`New board state is invalid: ${JSON.stringify(newState)}`)
+      }
+
+      return { boardState: newState }
+    }),
   }),
 ))
+
+type ResultsProps = {
+  win: Win | null
+  className?: string
+}
+
+function Results({ win, className = 'relative' }: ResultsProps) {
+  const [conceded, setConceded] = useState(false)
+  const goDeeper = useGameStore(state => state.goDeeper)
+
+  if (!win) return null
+
+  if (conceded) {
+    return (
+      <div className={className}>
+        <div className="absolute inset-0 bg-black opacity-50" />
+
+        <span>Game Over: {win.player} wins</span>
+      </div>
+    )
+  }
+
+  return (
+    <div className={className}>
+      <div className="absolute inset-0 bg-black opacity-50" />
+      <div className="absolute flex gap-2">
+        <button className="bg-white" onClick={() => setConceded(true)}>Concede</button>
+        <button className="bg-white" onClick={() => goDeeper()}>Go Deeper</button>
+      </div>
+    </div>
+  )
+}
 
 type Props = {
   parentPath?: number[]
@@ -30,9 +78,11 @@ export default function TicTacToeBoard({ parentPath = [], disabled = null }: Pro
   ))
   const boardState = pickNestedBoardState(fullBoardState, parentPath)
   const win = findWin(boardState)
+  // We only need to know if the whole game is over at the top level
+  const globalWin = parentPath.length <= 0 ? win : null
 
   return (
-    <div className="grid aspect-square w-full grid-cols-3 gap-px bg-black">
+    <div className="relative grid aspect-square w-full grid-cols-3 gap-px bg-black">
       {boardState.map((cell, i) => isBoardState(cell)
         ? (
             <TicTacToeBoard
@@ -53,6 +103,8 @@ export default function TicTacToeBoard({ parentPath = [], disabled = null }: Pro
               <span className="absolute">{cell}</span>
             </button>
           ))}
+
+      <Results win={globalWin} className="absolute inset-0" />
     </div>
   )
 }
