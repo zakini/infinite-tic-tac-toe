bb84e1602df772bde93caf82c66b2e646c60fc4d
Josh Wells <josh@parall.ax>
2025-04-12 11:11:14 +0100

    Removed buttons from completed sub-boards
    Also added simple aria labels to boards

--- app/components/tic-tac-toe-board/board.tsx
+++ app/components/tic-tac-toe-board/board.tsx
@@ -17,32 +17,35 @@ export default function Board({ parentPath = [], disabled = null }: Props) {
   const winCells = win ? win.cells : null
 
   return (
-    <div className="relative grid aspect-square w-full grid-cols-3 gap-px bg-black p-px">
-      {win && parentPath.length > 0 && (
-        <div className="absolute inset-0 z-10 m-px bg-green-500 opacity-80">{win.player}</div>
-      )}
-      {boardState.map((cell, i) => isBoardState(cell)
-        ? (
-            <Board
-              key={i}
-              parentPath={[...parentPath, i]}
-              disabled={disabled || !turnValid([...parentPath, i], turnPath)}
-            />
-          )
-        : (
-            <button
-              key={i}
-              className={`relative aspect-square ${winCells?.includes(i) ? 'bg-green-500' : 'bg-white disabled:bg-gray-400'}`}
-              disabled={disabled || win !== null || cell !== null}
-              onClick={() => {
-                // This is a bug in Typescript. Use 'as' to override
-                // See: https://github.com/microsoft/TypeScript/issues/60463
-                takeTurn([...parentPath, i] as unknown as [number, ...number[]])
-              }}
-            >
-              <span className="absolute">{cell}</span>
-            </button>
-          ))}
-    </div>
+    win && parentPath.length > 0
+      ? (
+          <section className="size-full bg-green-500" aria-label={`sub-board won by ${win.player}`}>{win.player}</section>
+        )
+      : (
+          <section className="grid aspect-square w-full grid-cols-3 gap-px bg-black p-px" aria-label="in-progress sub-board">
+            {boardState.map((cell, i) => isBoardState(cell)
+              ? (
+                  <Board
+                    key={i}
+                    parentPath={[...parentPath, i]}
+                    disabled={disabled || !turnValid([...parentPath, i], turnPath)}
+                  />
+                )
+              : (
+                  <button
+                    key={i}
+                    className={`relative aspect-square ${winCells?.includes(i) ? 'bg-green-500' : 'bg-white disabled:bg-gray-400'}`}
+                    disabled={disabled || win !== null || cell !== null}
+                    onClick={() => {
+                      // This is a bug in Typescript. Use 'as' to override
+                      // See: https://github.com/microsoft/TypeScript/issues/60463
+                      takeTurn([...parentPath, i] as unknown as [number, ...number[]])
+                    }}
+                  >
+                    <span className="absolute">{cell}</span>
+                  </button>
+                ))}
+          </section>
+        )
   )
 }
